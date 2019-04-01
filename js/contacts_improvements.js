82443ec25d66a99c795ceaebc517112d24b2ca82
Gabriel Giri <gabrielgiri@gmail.com>
2019-04-01 10:05:38 -0300

    Added modal when the user click on the checkbox

--- js/contacts_improvements.js
+++ js/contacts_improvements.js
@@ -213,6 +213,14 @@ function getURLParameter(name) {
     return rv ? decodeURI(rv) : rv;
 }
 
+function cancel_newsletter() {
+    modal.style.display = "none";
+}
+
+function save_newsletter() {
+    modal.style.display = "none";
+}
+
 $(document).ready(function() {
     var version = getURLParameter('v');
     if (version) {
@@ -230,3 +238,29 @@ $(document).ready(function() {
     update_table_status();
     update_current_sort({'field':'contact-name','direction':'asc'});
 });
+
+    // Get the modal
+    var modal = document.getElementById('myModal');
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+    
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+    
+    // When the user clicks on the button, open the modal 
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+    
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+    
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
